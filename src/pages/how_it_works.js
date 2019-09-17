cee4dc54e1d65315d1c39d5fe63e88a929825e44
ZePedroResende <j.p.resende68@gmail.com>
2019-09-17 21:44:30 +0100

    navbar

---
+++ src/pages/how_it_works.js
@@ -0,0 +1,5 @@
+import React from "react"
+import coverflex from "../images/how_it_works/coverflex.svg"
+import "../styles/how_it_works.sass"
+
+export default () => <div></div>
---
+++ src/pages/navbar.js
@@ -0,0 +1,45 @@
+import React from "react"
+import coverflex from "../images/how_it_works/coverflex.svg"
+import "../styles/how_it_works.sass"
+
+export default () => (
+  <div>
+    <nav
+      class="navbar is-transparent"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src={coverflex}></img>
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="cfNavbar"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="cfNavbar" class="navbar-menu">
+        <div class="navbar-end">
+          <a class="navbar-item">About</a>
+          <a class="navbar-item">How it works</a>
+          <a class="navbar-item">Plans</a>
+          <a class="navbar-item">Support</a>
+          <div class="navbar-item">
+            <a class="button is-primary">
+              <div class="text-button">Dashboard</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+)
